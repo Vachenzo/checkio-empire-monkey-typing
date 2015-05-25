2aa45476877c012761b1df101c4940b3a401b50d
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-05-25 16:07:40 +0300

    fix cover

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -6,13 +6,17 @@ import settings_env
 from tests import TESTS
 
 
+py_cover = """def cover(data, f):
+    return f(data[0], set(data[1]))
+"""
+
+
 class Referee(RefereeBase):
     TESTS = TESTS
     ENVIRONMENTS = settings_env.ENVIRONMENTS
 
     DEFAULT_FUNCTION_NAME = "count_words"
     ENV_COVERCODE = {
-        "python_2": covercodes.py_2_str,
-        "python_3": None,
+        "python_3": py_cover,
         "javascript": None
     }
