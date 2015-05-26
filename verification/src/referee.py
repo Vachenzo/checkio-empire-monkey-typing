afdb44c4905adf39c892fb0a4b6dc4bf94a6b731
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-05-26 09:56:06 +0300

    Add representation and fix cover

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -1,12 +1,15 @@
 from checkio_referee import RefereeBase
-from checkio_referee import covercodes
+from checkio_referee import covercodes, representations
 
 
 import settings_env
 from tests import TESTS
 
 
-py_cover = """def cover(data, f):
+def py_representation(test, function_name):
+    return '{}("{}", {})'.format(function_name, test["input"][0], set(test["input"][1]))
+
+py_cover = """def cover(f, data):
     return f(data[0], set(data[1]))
 """
 
@@ -19,4 +22,7 @@ class Referee(RefereeBase):
     ENV_COVERCODE = {
         "python_3": py_cover,
         "javascript": None
+    }
+    CALLED_REPRESENTATIONS = {
+        "python_3": py_representation,
     }
